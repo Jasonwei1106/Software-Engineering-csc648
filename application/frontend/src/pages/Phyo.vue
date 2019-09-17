4d3df0dd8e6c82b05678c4d81f5b4e4f75cf96aa
Htut <phyo.htut@windriver.com>
2019-09-17 13:20:53 -0700

    merged jason, myles, and antonio

--- application/frontend/src/pages/Phyo.vue
+++ application/frontend/src/pages/Phyo.vue
@@ -3,20 +3,28 @@
     <div>
       <q-card>
         <q-card-section>
-      <q-avatar size = "200px"
-      style= "padding-top: 40px; margin-left: 42%">
-      <q-img
-        src="../statics/test.jpg"
-        class = "avatar"
-        style="max-width: 400px; height: 200px;"
-      >
-      </q-img>
-      </q-avatar>
-      </q-card-section>
+          <q-avatar
+            size="200px"
+            style="
+              padding-top: 40px;
+              margin-left: 42%;
+            "
+          >
+            <q-img
+              src="../statics/square_phyo.jpg"
+              class="avatar"
+              style="
+                max-width: 400px;
+                height: 200px;
+              "
+            />
+          </q-avatar>
+        </q-card-section>
       </q-card>
     </div>
+
     <div style="padding: 40px; margin: 100px">
-    summary:
+      {{ intro }}
     </div>
   </div>
 </template>
@@ -25,7 +33,7 @@
 export default {
   data () {
     return {
-      name: null
+      intro: `Hi, I'm Phyo Htut. I'm currently a graduate student for Energy Systems [MS] and in process of transferring to Computer Science [MS] at San Francisco State University (SFSU). I am the project lead for this class project.`
     }
   }
 }
