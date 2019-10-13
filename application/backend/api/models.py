4fa916f6b920d6b538559f7879807b26608091f9
mylespedronan <myles.pedronan@gmail.com>
2019-10-13 01:40:08 -0700

    Added tutorial routes and current user route.

--- application/backend/api/models.py
+++ application/backend/api/models.py
@@ -6,16 +6,32 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
     is_admin = db.Column(db.Boolean)
 
-class Tutorials(db.Model):
+class Tutorial(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60), nullable=False)
-    user = db.Column(db.String(20), nullable=False)
+    image = db.Column(db.String(120), nullable=False)
+    category = db.Column(db.String(20), nullable=False)
     description = db.Column(db.String(120), nullable=False)
+    author_difficulty = db.Column(db.Integer, nullable=False)
+    viewer_difficulty = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
+    username = db.Column(db.String(80))
 
-class Steps(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Step(db.Model):
+    index = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(120), nullable=False)
     image = db.Column(db.String(80), nullable=False)
     tutorial = db.Column(db.Integer)
     next = db.Column(db.Integer)
     previous = db.Column(db.Integer)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(120), nullable=False)
+    edited = db.Column(db.Boolean)
+    imaage = db.Column(db.String(120))
+
+class List(db.Model):
+    title = db.Column(db.String(20), primary_key=True)
+    content = db.Column(db.String(120), nullable=False)
+    links = db.Column(db.String(120))
--- application/backend/api/routes.py
+++ application/backend/api/routes.py
@@ -1,12 +1,13 @@
 from flask import request, jsonify, make_response
 from api import app, db
-from api.models import User, Tutorials, Steps
+from api.models import User, Tutorial, Step, Comment, List
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
 from functools import wraps
 
+# Token used to authenticate user
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -28,11 +29,18 @@ def token_required(f):
 
     return decorated
 
-## Functions to make
-# get_all
-# get_one
-# create_
-# delete_
+#################
+### HOME PAGE ###
+#################
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return jsonify({'message': 'Hello World!'})
+
+######################
+### USER FUNCTIONS ###
+######################
 
 @app.route('/user', methods=['GET'])
 @token_required
@@ -74,6 +82,22 @@ def get_one_user(current_user, email_address):
     user_data['is_admin'] = user.is_admin
 
     return jsonify({'users' : user_data})
+
+@app.route('/user/current_user', methods=['GET'])
+@token_required
+def get_current_user(current_user):
+
+    user = User.query.filter_by(email_address=current_user.email_address).first()
+
+    if not user:
+        return jsonify({'message' : 'No user found!'})
+
+    user_data = {}
+    user_data['email_address'] = current_user.email_address
+    user_data['username'] = current_user.username
+    user_data['is_admin'] = current_user.is_admin
+
+    return jsonify({'current user' : user_data})
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -140,3 +164,83 @@ def login():
         return jsonify({'token' : token.decode('UTF-8')})
 
     return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+##########################
+### TUTORIAL FUNCTIONS ###
+##########################
+
+@app.route('/tutorial', methods=['GET'])
+@token_required
+def get_all_tutorials(current_user):
+    tutorials = Tutorial.query.all()
+
+    output = []
+
+    for tutorial in tutorials:
+        tutorial_data = {}
+        tutorial_data['id'] = tutorial.id
+        tutorial_data['title'] = tutorial.title
+        tutorial_data['image'] = tutorial.image
+        tutorial_data['description'] = tutorial.description
+        tutorial_data['author_difficulty'] = tutorial.author_difficulty
+        tutorial_data['viewer_difficulty'] = tutorial.viewer_difficulty
+        tutorial_data['rating'] = tutorial.rating
+        tutorial_data['username'] = tutorial.username
+        output.append(tutorial_data)
+
+    return jsonify({'tutorials' : output})
+
+
+
+@app.route('/tutorial/<username>/<tutorial_id>', methods=['GET'])
+def get_one_tutorial(username, tutorial_id):
+
+    tutorial = Tutorial.query.filter_by(username=username, id=tutorial_id).first()
+
+    if not tutorial:
+        return jsonify({'message' : 'No tutorial found!'})
+
+    tutorial_data = {}
+    tutorial_data['id'] = tutorial.id
+    tutorial_data['title'] = tutorial.title
+    tutorial_data['image'] = tutorial.image
+    tutorial_data['description'] = tutorial.description
+    tutorial_data['author_difficulty'] = tutorial.author_difficulty
+    tutorial_data['viewer_difficulty'] = tutorial.viewer_difficulty
+    tutorial_data['rating'] = tutorial.rating
+    tutorial_data['username'] = tutorial.username
+
+    return jsonify({'tutorial' : tutorial_data})
+
+@app.route('/tutorial', methods=['POST'])
+@token_required
+def create_tutorial(current_user):
+    data = request.get_json()
+
+    new_tutorial = Tutorial(title=data['title'], image=data['image'], category=data['category'], description=data['description'], author_difficulty=data['author_difficulty'], username=current_user.username)
+
+    db.session.add(new_tutorial)
+    db.session.commit()
+
+    return jsonify({'message' : 'New tutorial created!'})
+
+@app.route('/tutorial/<username>/<tutorial_id>', methods=['DELETE'])
+@token_required
+def delete_tutorial(current_user, username, tutorial_id):
+
+    if not current_user.is_admin:
+        return jsonify({'message' : 'Not an admin. Cannot perform that function!'})
+
+    tutorial = Tutorial.query.filter_by(id=tutorial_id, user_email=username).first()
+
+    if not tutorial:
+        return jsonify({'message' : 'No tutorial found!'})
+
+    db.session.delete(tutorial)
+    db.session.commit()
+
+    return jsonify({'message' : 'Tutorial been deleted'})
+
+######################
+### STEP FUNCTIONS ###
+######################
