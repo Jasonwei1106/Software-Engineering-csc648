fda89ce9cfb07cd5783f361e6d73e35366bf9d82
mylespedronan <myles.pedronan@gmail.com>
2019-10-17 21:39:05 -0700

    Added additional routes and tutorials show steps associated

--- application/backend/api/models.py
+++ application/backend/api/models.py
@@ -6,6 +6,10 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
     is_admin = db.Column(db.Boolean)
 
+    # Relationships
+    tutorials = db.relationship('Tutorial', backref='tutorial_owner')
+    steps = db.relationship('Step', backref='user_step')
+
 class Tutorial(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60), nullable=False)
@@ -15,15 +19,23 @@ class Tutorial(db.Model):
     author_difficulty = db.Column(db.Integer, nullable=False)
     viewer_difficulty = db.Column(db.Integer)
     rating = db.Column(db.Integer)
-    username = db.Column(db.String(80))
+
+    # ForeignKey
+    username = db.Column(db.String(80), db.ForeignKey('user.username'))
+
+    # Relationships
+    steps = db.relationship('Step', backref='tutorial_steps')
+    comments = db.relationship('Comment', backref='tutorial_comments')
+    lists = db.relationship('List', backref='tutorial_lists')
 
 class Step(db.Model):
     index = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(120), nullable=False)
     image = db.Column(db.String(80), nullable=False)
-    tutorial = db.Column(db.Integer)
-    next = db.Column(db.Integer)
-    previous = db.Column(db.Integer)
+
+    # ForeignKey
+    tutorial = db.Column(db.Integer, db.ForeignKey('tutorial.id'))
+    user = db.Column(db.String(20), db.ForeignKey('user.username'))
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,7 +43,13 @@ class Comment(db.Model):
     edited = db.Column(db.Boolean)
     imaage = db.Column(db.String(120))
 
+    # ForeignKey
+    tutorial = db.Column(db.Integer, db.ForeignKey('tutorial.id'))
+
 class List(db.Model):
     title = db.Column(db.String(20), primary_key=True)
     content = db.Column(db.String(120), nullable=False)
     links = db.Column(db.String(120))
+
+    # ForeignKey
+    tutorial = db.Column(db.Integer, db.ForeignKey('tutorial.id'))
--- application/backend/api/routes.py
+++ application/backend/api/routes.py
@@ -29,6 +29,7 @@ def token_required(f):
 
     return decorated
 
+## TO GET RID OF IN DEPLOYMENT ##
 #################
 ### HOME PAGE ###
 #################
@@ -159,7 +160,7 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'email_address' : user.email_address, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        token = jwt.encode({'email_address' : user.email_address, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'])
 
         return jsonify({'token' : token.decode('UTF-8')})
 
@@ -177,6 +178,9 @@ def get_all_tutorials(current_user):
     output = []
 
     for tutorial in tutorials:
+        steps = Step.query.filter_by(tutorial=tutorial.id).all()
+        output_steps = []
+
         tutorial_data = {}
         tutorial_data['id'] = tutorial.id
         tutorial_data['title'] = tutorial.title
@@ -186,11 +190,55 @@ def get_all_tutorials(current_user):
         tutorial_data['viewer_difficulty'] = tutorial.viewer_difficulty
         tutorial_data['rating'] = tutorial.rating
         tutorial_data['username'] = tutorial.username
+
+        for step in steps:
+            step_data = {}
+            step_data['index'] = step.index
+            step_data['content'] = step.content
+            step_data['image'] = step.image
+            output_steps.append(step_data)
+
+        tutorial_data['steps'] = output_steps
+
         output.append(tutorial_data)
 
     return jsonify({'tutorials' : output})
 
+@app.route('/tutorial/<username>', methods=['GET'])
+def get_all_tutorials_by_user(username):
+    tutorials = Tutorial.query.filter_by(username=username).all()
 
+    if not tutorials:
+        return jsonify({'message' : 'No tutorial found!'})
+
+    output = []
+
+    for tutorial in tutorials:
+        steps = Step.query.filter_by(tutorial=tutorial.id).all()
+        output_steps = []
+
+        tutorial_data = {}
+        tutorial_data['id'] = tutorial.id
+        tutorial_data['title'] = tutorial.title
+        tutorial_data['image'] = tutorial.image
+        tutorial_data['description'] = tutorial.description
+        tutorial_data['author_difficulty'] = tutorial.author_difficulty
+        tutorial_data['viewer_difficulty'] = tutorial.viewer_difficulty
+        tutorial_data['rating'] = tutorial.rating
+        tutorial_data['username'] = tutorial.username
+
+        for step in steps:
+            step_data = {}
+            step_data['index'] = step.index
+            step_data['content'] = step.content
+            step_data['image'] = step.image
+            output_steps.append(step_data)
+
+        tutorial_data['steps'] = output_steps
+
+        output.append(tutorial_data)
+
+    return jsonify({'tutorials' : output})
 
 @app.route('/tutorial/<username>/<tutorial_id>', methods=['GET'])
 def get_one_tutorial(username, tutorial_id):
@@ -199,6 +247,9 @@ def get_one_tutorial(username, tutorial_id):
 
     if not tutorial:
         return jsonify({'message' : 'No tutorial found!'})
+
+    steps = Step.query.filter_by(tutorial=tutorial.id).all()
+    output_steps = []
 
     tutorial_data = {}
     tutorial_data['id'] = tutorial.id
@@ -210,6 +261,15 @@ def get_one_tutorial(username, tutorial_id):
     tutorial_data['rating'] = tutorial.rating
     tutorial_data['username'] = tutorial.username
 
+    for step in steps:
+        step_data = {}
+        step_data['index'] = step.index
+        step_data['content'] = step.content
+        step_data['image'] = step.image
+        output_steps.append(step_data)
+
+    tutorial_data['steps'] = output_steps
+
     return jsonify({'tutorial' : tutorial_data})
 
 @app.route('/tutorial', methods=['POST'])
@@ -217,7 +277,9 @@ def get_one_tutorial(username, tutorial_id):
 def create_tutorial(current_user):
     data = request.get_json()
 
-    new_tutorial = Tutorial(title=data['title'], image=data['image'], category=data['category'], description=data['description'], author_difficulty=data['author_difficulty'], username=current_user.username)
+    user = User.query.filter_by(username=current_user.username).first()
+
+    new_tutorial = Tutorial(title=data['title'], image=data['image'], category=data['category'], description=data['description'], author_difficulty=data['author_difficulty'], tutorial_owner=user)
 
     db.session.add(new_tutorial)
     db.session.commit()
@@ -227,11 +289,10 @@ def create_tutorial(current_user):
 @app.route('/tutorial/<username>/<tutorial_id>', methods=['DELETE'])
 @token_required
 def delete_tutorial(current_user, username, tutorial_id):
+    if username != current_user.username or current_user.is_admin == False:
+        return jsonify({'message' : 'Cannot delete tutorial of a different user!'}), 403
 
-    if not current_user.is_admin:
-        return jsonify({'message' : 'Not an admin. Cannot perform that function!'})
-
-    tutorial = Tutorial.query.filter_by(id=tutorial_id, user_email=username).first()
+    tutorial = Tutorial.query.filter_by(id=tutorial_id, username=username).first()
 
     if not tutorial:
         return jsonify({'message' : 'No tutorial found!'})
@@ -244,3 +305,75 @@ def delete_tutorial(current_user, username, tutorial_id):
 ######################
 ### STEP FUNCTIONS ###
 ######################
+
+@app.route('/tutorial/<username>/<tutorial_id>/step', methods=['GET'])
+def get_all_steps(username, tutorial_id):
+    steps = Step.query.filter_by(tutorial=tutorial_id, user=username).all()
+
+    if not steps:
+        return jsonify({'message' : 'No steps found for tutorial!'})
+
+    output = []
+
+    for step in steps:
+        step_data = {}
+        step_data['index'] = step.index
+        step_data['content'] = step.content
+        step_data['image'] = step.image
+        step_data['tutorial'] = step.tutorial
+        step_data['user'] = step.user
+        output.append(step_data)
+
+    return jsonify({'steps' : output})
+
+@app.route('/tutorial/<username>/<tutorial_id>/step/<step_index>', methods=['GET'])
+def get_one_step(username, tutorial_id, step_index):
+    steps = Step.query.filter_by(tutorial=tutorial_id, user=username, index=step_index).first()
+
+    if not steps:
+        return jsonify({'message' : 'No steps found for tutorial!'})
+
+    step_data = {}
+    step_data['index'] = step.index
+    step_data['content'] = step.content
+    step_data['image'] = step.image
+    step_data['tutorial'] = step.tutorial
+    step_data['user'] = step.user
+
+    return jsonify({'steps' : step_data})
+
+@app.route('/tutorial/<username>/<tutorial_id>/step', methods=['POST'])
+@token_required
+def create_tutorial_step(current_user, username, tutorial_id):
+    if username != current_user.username or current_user.is_admin == False:
+        return jsonify({'message' : 'Cannot create tutorial for a different user!'}), 403
+
+    data = request.get_json()
+
+    new_step = Step(content=data['content'], image=data['image'], tutorial=tutorial_id, user=username)
+
+    db.session.add(new_step)
+    db.session.commit()
+
+    return jsonify({'message' : 'Step has been created'})
+
+@app.route('/tutorial/<username>/<tutorial_id>/step/<step_index>', methods=['DELETE'])
+@token_required
+def delete_tutorial_step(current_user, username, tutorial_id, step_index):
+    if username != current_user.username or current_user.is_admin == False:
+        return jsonify({'message' : 'Cannot delete tutorial step for a different user!'}), 403
+
+    step = Step.query.filter_by(user=username, tutorial=tutorial_id, index=step_index).first()
+
+    if not step:
+        return jsonify({'message' : 'No step found for tutorial!'})
+
+    db.session.delete(step)
+    db.session.commit()
+
+    return jsonify({'message' : 'Step has been deleted'})
+
+# Test list
+@app.route('/test', methods=['GET'])
+def test_route():
+    return ''
